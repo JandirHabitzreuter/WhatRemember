6c5b6f2969a30e73496aed5c586312e1186eeaa4
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2023-02-08 20:43:17 -0300

    feat: creating use case theme

--- backEnd/src/infra/container/index.ts
+++ backEnd/src/infra/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from "tsyringe";
 
+import { ThemeRepositoryPrisma } from "@modules/infra/repositories/ThemeRepositoryPrisma";
 import { UserRepositoryPrisma } from "@modules/infra/repositories/UserRepositoryPrisma";
 
 container.registerSingleton("UserRepositoryPrisma", UserRepositoryPrisma);
+container.registerSingleton("ThemeRepositoryPrisma", ThemeRepositoryPrisma);
